9af89c18e7955954a19a26c189bc803fad912dd9
Mickey Muldoon <muldoon.mickey@gmail.com>
2019-03-16 18:57:07 -0700

    Fixed query params

--- scripts/redux/actions.js
+++ scripts/redux/actions.js
@@ -765,6 +765,54 @@ const userActions = {
       });
   },
 
+  checkEmailSignin: ()=> {
+      // Confirm the link is a sign-in with email link.
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      // Additional state parameters can also be passed via URL.
+      // This can be used to continue the user's intended action before triggering
+      // the sign-in operation.
+      // Get the email if available. This should be available if the user completes
+      // the flow on the same device where they started it.
+      var email = window.localStorage.getItem('emailForSignIn');
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt('Please provide your email for confirmation');
+      }
+      const emailRegularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const validEmail = emailRegularExpression.test(email);
+      if (validEmail) {
+      // The client SDK will parse the code from the link for you.
+      firebase.auth().signInWithEmailLink(email, window.location.href)
+        .then(function (result) {
+          // Clear email from storage.
+          console.log(result);
+          window.localStorage.removeItem('emailForSignIn');
+          // this.set('queryParams', null);        
+
+          // You can access the new user via result.user
+          // Additional user info profile not available via:
+          // result.additionalUserInfo.profile == null
+          // You can check if the user is new or existing:
+          // result.additionalUserInfo.isNewUser
+        })
+        .then(() => {
+          console.log("i'd like to remove queryParams now");
+        })
+        .catch(function (error) {
+          // Some error occurred, you can inspect the code: error.code
+          // Common errors could be invalid email and invalid or expired OTPs.
+          console.log(error);
+          alert(error);
+        });
+      }
+      // if (this.user && this.user.email) {
+      //   this.dispatch(userActions.isRegistered(this.user.email));         
+      // }
+    }
+
+  },
+
   signOut: () => {
     return firebase.auth()
       .signOut()
